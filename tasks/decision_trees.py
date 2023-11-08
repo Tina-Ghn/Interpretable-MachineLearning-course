fe864a89e0be994bd3816417b68fb1c1afebfcdb
Fateme <fateme.ghoochani@gmail.com>
2023-11-08 13:53:50 +0100

    Update decision_trees.py

--- tasks/decision_trees.py
+++ tasks/decision_trees.py
@@ -3,10 +3,8 @@ sys.path.insert(0, "")
 
 import numpy as np
 from sklearn import tree
-
 from tasks.plotting import plot_bar
 from utils.dataset import Dataset
-
 
 def fit_decision_tree(X_train, y_train, random_state=0):
     """
@@ -21,11 +19,9 @@ def fit_decision_tree(X_train, y_train, random_state=0):
     Returns:
         model (DecisionTreeClassifier): Fitted decision tree.
     """
-    
-    model = None
-    
+    model = tree.DecisionTreeClassifier(random_state=random_state)
+    model.fit(X_train, y_train)
     return model
-
 
 def plot_feature_importance(model, dataset, title=None):
     """
@@ -42,14 +38,14 @@ def plot_feature_importance(model, dataset, title=None):
         y (list): Values, which are displayed on the y-axis.
     
     """
-    x = None
-    y = None
-    
+    x = [dataset.get_input_labels(i) for i in range(len(dataset.input_ids))]  # Use the method to get feature labels
+    y = model.feature_importances_
+    plot_bar(x, y, name=title, ylabel="Feature Importance")
     return x, y
-
 
 def compute_feature_importance(model):
     """
+    3.3
     Computes the feature importance of DecisionTreeClassifier from scratch.
     
     Inputs:
@@ -58,22 +54,26 @@ def compute_feature_importance(model):
     Returns:
         feature_importance (np.ndarray): Feature importances with shape (#features,).
     """
-    
-    children_left = model.tree_.children_left  # id of the left child of node i or -1 if leaf node
-    children_right = model.tree_.children_right  # id of the right child of node i or -1 if leaf node
-    feature = model.tree_.feature  # Feature used for splitting node i
-    num_features = model.tree_.n_features  # Number of features
     impurity = model.tree_.impurity  # The impurity at node i
     samples = model.tree_.weighted_n_node_samples  # Weighted number of training samples reaching node i
 
     # Initialize the feature importance with zeros and shape (num_features,)
-    feature_importance = np.zeros((num_features,))
-    
-    return feature_importance
+    feature_importance = np.zeros((model.tree_.n_features,))
 
+    # Calculate the feature importance
+    for node in range(model.tree_.node_count):
+        if model.tree_.children_left[node] != model.tree_.children_right[node]:
+            left = model.tree_.children_left[node]
+            right = model.tree_.children_right[node]
+            feature = model.tree_.feature[node]
+            decrease = impurity[node] - (samples[left] * impurity[left] + samples[right] * impurity[right]) / samples[node]
+            feature_importance[feature] += decrease * samples[node]
+
+    return feature_importance
 
 def normalize_feature_importance(feature_importance):
     """
+    3.4
     Normalizes the given feature importances.
     
     Inputs:
@@ -82,21 +82,16 @@ def normalize_feature_importance(feature_importance):
     Returns:
         feature_importance (np.ndarray): Normalized feature importances with shape (#features,).
     """
-        
-    return feature_importance
-    
+    total_importance = np.sum(feature_importance)
+    normalized_importance = feature_importance / total_importance
+    return normalized_importance
 
 if __name__ == "__main__":
     dataset = Dataset("wheat_seeds", [0,1,2,3,4,5,6], [7], normalize=True, categorical=True)
     (X_train, y_train), (X_test, y_test) = dataset.get_data()
-
-    model = fit_decision_tree(X_train, y_train)
     
-    plot_feature_importance(
-        model, dataset, title="Decision Tree")
+    model = fit_decision_tree(X_train, y_train)
+    plot_feature_importance(model, dataset, title="Decision Tree")
     
     fi = compute_feature_importance(model)
     fi = normalize_feature_importance(fi)
-
-        
-    
