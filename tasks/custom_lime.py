347a580577e4bc1d55e1a42f015b0ea3a0a5c973
Fateme <fateme.ghoochani@gmail.com>
2023-12-06 16:55:28 +0100

    Update custom_lime.py

--- tasks/custom_lime.py
+++ tasks/custom_lime.py
@@ -10,7 +10,6 @@ import numpy as np
 import itertools
 from sklearn import tree
 
-
 def get_mesh(model, dataset, points_per_feature=50):
     """
     Retrieve mesh data for plotting a two-dimensional graph with `points_per_feature` for each axis.
@@ -28,49 +27,60 @@ def get_mesh(model, dataset, points_per_feature=50):
         M (np.ndarray): MLP predictions with shape (`points_per_feature`, `points_per_feature`).
     """
 
-    return None
+    # Get the lower and upper bounds for the first two features
+    lower_bound_1, upper_bound_1 = dataset.get_lower_and_upper_bound_for_feature(0)
+    lower_bound_2, upper_bound_2 = dataset.get_lower_and_upper_bound_for_feature(1)
+
+    # Generate equidistant points for x1 and x2
+    x1 = np.linspace(lower_bound_1, upper_bound_1, points_per_feature)
+    x2 = np.linspace(lower_bound_2, upper_bound_2, points_per_feature)
+
+    # Create a meshgrid
+    X1, X2 = np.meshgrid(x1, x2)
+
+    # Flatten the meshgrid to obtain points for prediction
+    Z = np.c_[X1.ravel(), X2.ravel()]
+
+    # Predict using the model
+    predictions = model.predict(Z)
+
+    # Reshape the predictions to match the shape of the meshgrid
+    M = predictions.reshape(X1.shape)
+
+    return x1, x2, M
+
 
 
 def plot_mesh(x1, x2, M, labels, title=None, embedded=False):
-    """
-    Uses the mesh data to add a color mesh to the plot.
-
-    Parameters:
-        x1 (np.ndarray): x-axis points with shape (N,).
-        x2 (np.ndarray): y-axis points with shape (N,).
-        M (np.ndarray): Color values with shape (N, N).
-        labels (list): Labels for x and y axis.
-        title (str): Title for the plot.
-        embedded (bool): Whether a new figure should be created or not.
-
-    Returns: 
-        plt (matplotlib.pyplot or utils.styled_plot.plt): Plot with applied color mesh.
-    """
-
     if not embedded:
         plt.figure()
+
+    plt.pcolormesh(x1, x2, M, cmap='coolwarm', shading='auto')
+    plt.colorbar(label='Prediction')
+
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+
+    if title:
+        plt.title(title)
 
     return plt
 
 
+
+
+
+
 def sample_points(model, dataset, num_points, seed=0):
-    """
-    Samples points for the two first features. Uses the bounds from configspace again.
-
-    Parameters:
-        model: Classifier which can call a predict method.
-        dataset (utils.Dataset): dataset (utils.Dataset): Dataset with access to features and their bounds.
-        num_points (int): How many points should be sampled.
-        seed (int): Seed to feed random.
-
-    Returns:
-        Z (np.ndarray): Sampled points with shape (`num_points`, 2)
-        y (np.ndarray): MLP predictions on sampled data with shape (`num_points`,)
-    """
-
     random.seed(seed)
+    X = np.random.uniform(low=dataset.get_lower_and_upper_bound_for_feature(0)[0],
+                          high=dataset.get_lower_and_upper_bound_for_feature(0)[1],
+                          size=(num_points, 1))
+    y = model.predict(X)
+    return X, y
 
-    return None
+
+
 
 
 def weight_points(selected_x, Z, sigma=0.2):
@@ -86,7 +96,16 @@ def weight_points(selected_x, Z, sigma=0.2):
         weights (np.ndarray): Normalized weights between 0..1 with shape (?,).
     """
 
-    return None
+    # Calculate Euclidean distances
+    distances = np.linalg.norm(Z - selected_x, axis=1)
+
+    # Calculate weights using the given formula
+    weights = np.exp(-distances**2 / (2 * sigma**2))
+
+    # Normalize weights to be between 0 and 1
+    weights = weights / np.sum(weights)
+
+    return weights
 
 
 def plot_points_in_mesh(plt, X=[], y=[], weights=None, colors={}, selected_x=None, size=8):
@@ -98,29 +117,39 @@ def plot_points_in_mesh(plt, X=[], y=[], weights=None, colors={}, selected_x=Non
         X (np.ndarray): Points with shape (?, 2).
         y (np.ndarray): Target values with shape (?,).
         weights (np.ndarray): Normalized weights with shape (?,).
-        colors (dict): Returns the color for an y value.
+        colors (dict): Returns the color for a y value.
         selected_x (np.ndarray): Single point with shape (2,).
         size (int): Default size of the markers.
     """
 
-    return
+    # Scatter points from X with colors based on y
+    if X.shape[1] == 2:
+        scatter = plt.scatter(X[:, 0], X[:, 1], c=[colors[val] for val in y], s=weights * size)
+    else:
+        # Handle the case where X has more than 2 columns
+        # You may need to modify this based on your data structure
+        scatter = plt.scatter(X[:, 0], X[:, 1], c=[colors[val] for val in y], s=weights * size)
 
+    # Highlight the selected point
+    if selected_x is not None:
+        plt.scatter(selected_x[0], selected_x[1], color='red', marker='x', s=100, label='Selected Point')
+
+    # Add legend and labels if needed
+    plt.legend()
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+
+    return scatter
+
+
+
+from sklearn.tree import DecisionTreeRegressor
 
 def fit_explainer_model(Z, y, weights=None, seed=0):
-    """
-    Fits a decision tree.
+    model = tree.DecisionTreeRegressor(random_state=seed)
+    model.fit(Z, y, sample_weight=weights)
 
-    Parameters:
-        Z (np.ndarray): Sampled points with shape (?, 2).
-        y (np.ndarray): MLP predictions values with shape (?,).
-        weights (np.ndarray): Normalized weights with shape (?,).
-        seed (int): Seed for the decision tree.
-
-    Returns:
-        model (DecisionTreeRegressor): Fitted explainer model.
-    """
-
-    return None
+    return model
 
 
 if __name__ == "__main__":
